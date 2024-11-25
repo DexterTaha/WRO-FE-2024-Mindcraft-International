2a332eed07e948924e6d59015bacc90fbfbcc0d7
TAIDI LAAMIRI Taha <130682580+DexterTaha@users.noreply.github.com>
2024-11-25 23:17:06 +0100

    Update test2.ino

--- src ( source code )/Open Challenge/test2.ino
+++ src ( source code )/Open Challenge/test2.ino
@@ -4,12 +4,9 @@
 #define I2C_ADDRESS 0x08  // I2C address of this Arduino
 #define SWITCH_PIN A0     // Switch pin
 
-// Buffer for storing the incoming message
-#define BUFFER_SIZE 64
-char receivedData[BUFFER_SIZE];  // Fixed-size buffer
-int receivedIndex = 0;           // Index in the buffer
-bool receiving = false;          // Flag to check if we are in the middle of receiving a message
-unsigned long lastReceiveTime = 0;   // Time when the last data was received
+String receivedData = "";          // String to store the incoming message
+bool receiving = false;            // Flag to check if we are in the middle of receiving a message
+unsigned long lastReceiveTime = 0; // Time when the last data was received
 unsigned long timeoutDuration = 500; // Timeout duration in milliseconds
 
 // Motor control pins
@@ -32,25 +29,13 @@ int FR = 0, FL = 0, L1 = 0, L2 = 0, R1 = 0, R2 = 0;
 // Variable to count increments
 int counter = 0;
 unsigned long lastCounterUpdateTime = 0; // Time when the counter was last incremented
-const unsigned long counterDelay = 5000; // 5 seconds in milliseconds
+const unsigned long counterDelay = 3000; // 5 seconds in milliseconds
 
 // Function to parse the received message
 void parseMessage(String message) {
   if (message.startsWith("-") && message.endsWith(".")) {
     message = message.substring(1, message.length() - 1);
 
-    int expectedCommas = 5;
-    int commaCount = 0;
-    for (char c : message) {
-      if (c == ',') commaCount++;
-    }
-
-    if (commaCount != expectedCommas) {
-      Serial.println("Error: Incorrect number of data fields");
-      return;
-    }
-
-    // Proceed with parsing
     int firstComma = message.indexOf(',');
     int secondComma = message.indexOf(',', firstComma + 1);
     int thirdComma = message.indexOf(',', secondComma + 1);
@@ -85,15 +70,11 @@ void BuzzerRobotEnd() {
 }
 
 void BuzzerRobotError() {
-  static bool errorBuzzerPlayed = false;
-  if (!errorBuzzerPlayed) {
-    for (int i = 0; i < 5; i++) {
-      tone(buzzerPin, 1500);
-      delay(100);
-      noTone(buzzerPin);
-      delay(100);
-    }
-    errorBuzzerPlayed = true;
+  for (int i = 0; i < 5; i++) {
+    tone(buzzerPin, 1500);
+    delay(100);
+    noTone(buzzerPin);
+    delay(100);
   }
 }
 
@@ -123,19 +104,7 @@ void HoldMotors() {
 
 // Check if switch is on
 bool isSwitchOn(int value) {
-  static unsigned long lastDebounceTime = 0;
-  static bool lastSwitchState = false;
-
-  bool currentSwitchState = value > 1000;
-  if (currentSwitchState != lastSwitchState) {
-    lastDebounceTime = millis();
-  }
-
-  if ((millis() - lastDebounceTime) > 50) { // 50 ms debounce time
-    lastSwitchState = currentSwitchState;
-  }
-
-  return lastSwitchState;
+  return value > 1000;
 }
 
 // Handle data received over I2C
@@ -143,26 +112,18 @@ void receiveEvent(int bytesReceived) {
   while (Wire.available()) {
     char c = Wire.read();
     if (c == '-') {
-      receivedIndex = 0;
-      memset(receivedData, 0, BUFFER_SIZE);
-      receivedData[receivedIndex++] = c;
+      receivedData = "-";
       receiving = true;
     } else if (c == '.' && receiving) {
-      receivedData[receivedIndex++] = c;
+      receivedData += ".";
       receiving = false;
       lastReceiveTime = millis();
-      receivedData[receivedIndex] = '\0'; // Null-terminate the string
-    } else if (receiving && receivedIndex < BUFFER_SIZE - 1) {
-      receivedData[receivedIndex++] = c;
     } else if (receiving) {
-      // Buffer full, handle the error
-      Serial.println("Error: Received data too long");
-      receiving = false;
-      receivedIndex = 0;
+      receivedData += c;
     }
   }
 
-  if (!receiving && receivedIndex > 0) {
+  if (!receiving && receivedData.length() > 0) {
     lastReceiveTime = millis();
   }
 }
@@ -170,12 +131,11 @@ void receiveEvent(int bytesReceived) {
 // Print LIDAR data
 void printLidarData() {
   if (millis() - lastReceiveTime <= timeoutDuration) {
-    if (receivedIndex > 0) {
+    if (receivedData.length() > 0) {
       Serial.println(receivedData);
     }
   } else {
-    memset(receivedData, 0, BUFFER_SIZE);
-    receivedIndex = 0;
+    receivedData = "";
     Serial.println("0,0,0,0,0,0.");
     BuzzerRobotSpecial();
     StopMotors();
@@ -197,52 +157,52 @@ void controlRobot(int speedInput, int steeringInput) {
   }
 
   analogWrite(ENA, motorSpeed);
-  steeringAngle = map(steeringInput, -100, 100, 60, 120); // Adjusted mapping
+  steeringAngle = map(steeringInput, -100, 100, 120, 60);
   STEERING.write(steeringAngle);
 }
 
 // Adjust control based on distance
 void adjustControlBasedOnDistance() {
   int R = 20;
-  int tolerance = 10;
-  unsigned long currentMillis = millis();
+  int tolerance = 5;
 
-  if (FR < 15 || FL < 15) {
-    Serial.println("stepBack");
-    stepBack();
-  } else if (R1 > 150 && R2 > 150) {
-    if (currentMillis - lastCounterUpdateTime > counterDelay) {
-      counter++;
-      lastCounterUpdateTime = currentMillis;
-
-      // Print the updated counter value
-      Serial.print("Counter: ");
-      Serial.println(counter);
-    }
+  if (R1 > 150 && R2 > 150) {
     controlRobot(50, 100);
+  } else if (R1 > 150 && R2 > 150 && millis() - lastCounterUpdateTime > counterDelay) {
+    counter++;
+    lastCounterUpdateTime = millis();
+
+    // Print the updated counter value
+    Serial.print("Counter: ");
+    Serial.println(counter);
+
+    delay(counterDelay); // Pause for 5 seconds
   } else if (abs(R1 - R2) <= tolerance && abs(R1 - R) <= tolerance) {
     controlRobot(100, 0);
   } else if (R1 < R && R2 < R && R1 < R2) {
-    controlRobot(50, 0);
+    controlRobot(30, 0);
   } else if (R2 < R1 && R1 < R && R2 < R) {
-    controlRobot(50, -80);
+    controlRobot(30, -80);
   } else if (R < R1 && R1 < R2 && R < R2) {
-    controlRobot(50, 80);
+    controlRobot(30, 80);
   } else if (R < R2 && R2 < R1 && R < R1) {
-    controlRobot(50, -80);
+    controlRobot(30, -80);
+  } else if (FR < 15 || FL < 15) {
+    Serial.println("stepBack");
+    stepBack();
   } else {
     controlRobot(30, 0);
   }
 }
 
 void stepBack() {
-  controlRobot(-100, 0); // Reverse straight back
+  controlRobot(-100, -100);
 }
 
 void act() {
   printLidarData();
   if (millis() - lastReceiveTime <= timeoutDuration) {
-    parseMessage(String(receivedData));
+    parseMessage(receivedData);
     adjustControlBasedOnDistance();
   } else {
     StopMotors();
@@ -267,27 +227,18 @@ void setup() {
   Serial.println("on");
 }
 
-bool lastSwitchState = false;
 void loop() {
   int switchValue = analogRead(SWITCH_PIN);
-  bool currentSwitchState = isSwitchOn(switchValue);
 
-  if (currentSwitchState) {
-    if (!lastSwitchState) {
-      Serial.println("Switch turned ON");
-    }
+  if (isSwitchOn(switchValue)) {
     act();
   } else {
-    if (lastSwitchState) {
-      Serial.println("Switch turned OFF");
-      BuzzerRobotError();
-    }
     StopMotors();
     controlRobot(0, 0);
+    BuzzerRobotError();
     Serial.println("ready");
+
   }
 
-  lastSwitchState = currentSwitchState;
   delay(100);
 }
-
