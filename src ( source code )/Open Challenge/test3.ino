1bf030b9ee3573826d8a3e554a2d4878c0421f41
TAIDI LAAMIRI Taha <130682580+DexterTaha@users.noreply.github.com>
2024-11-26 01:55:36 +0100

    Update test3.ino

--- src ( source code )/Open Challenge/test3.ino
+++ src ( source code )/Open Challenge/test3.ino
@@ -21,7 +21,7 @@ Servo STEERING;                // Create Servo object for steering
 // Motor control variables
 int maxSpeed = 255;
 int minSpeed = 127;
-int steeringAngle = 90; // Neutral steering position
+int steeringAngle = 0;
 
 // Variables to store the parsed values
 int FR = 0, FL = 0, L1 = 0, L2 = 0, R1 = 0, R2 = 0;
@@ -29,11 +29,7 @@ int FR = 0, FL = 0, L1 = 0, L2 = 0, R1 = 0, R2 = 0;
 // Variable to count increments
 int counter = 0;
 unsigned long lastCounterUpdateTime = 0; // Time when the counter was last incremented
-const unsigned long counterDelay = 3000; // 3 seconds in milliseconds
-
-// Debounce timing
-unsigned long lastSwitchCheck = 0;
-const unsigned long debounceDelay = 50;
+const unsigned long counterDelay = 3000; // 5 seconds in milliseconds
 
 // Function to parse the received message
 void parseMessage(String message) {
@@ -126,6 +122,24 @@ void receiveEvent(int bytesReceived) {
       receivedData += c;
     }
   }
+
+  if (!receiving && receivedData.length() > 0) {
+    lastReceiveTime = millis();
+  }
+}
+
+// Print LIDAR data
+void printLidarData() {
+  if (millis() - lastReceiveTime <= timeoutDuration) {
+    if (receivedData.length() > 0) {
+      Serial.println(receivedData);
+    }
+  } else {
+    receivedData = "";
+    Serial.println("0,0,0,0,0,0.");
+    BuzzerRobotSpecial();
+    StopMotors();
+  }
 }
 
 // Control robot motors
@@ -140,16 +154,18 @@ void controlRobot(int speedInput, int steeringInput) {
     digitalWrite(IN_2, HIGH);
   } else {
     StopMotors();
-    return;
   }
 
   analogWrite(ENA, motorSpeed);
-  steeringAngle = map(steeringInput, -100, 100, 60, 120);
+  steeringAngle = map(steeringInput, -100, 100, 120, 60);
   STEERING.write(steeringAngle);
 }
 
 // Adjust control based on distance
 void adjustControlBasedOnDistance() {
+  int R = 20;
+  int tolerance = 5;
+
   if (R2 > 100) {
     controlRobot(30, 50);
   } else if (R2 > 150 && millis() - lastCounterUpdateTime > counterDelay) {
@@ -159,22 +175,28 @@ void adjustControlBasedOnDistance() {
     // Print the updated counter value
     Serial.print("Counter: ");
     Serial.println(counter);
+
+    delay(counterDelay); // Pause for 5 seconds
   } else if (R2 >= 25 && R2 <= 35) {
     controlRobot(30, 0);
   } else if (R2 < 30) {
     controlRobot(30, -50);
   } else if (R2 > -30) {
     controlRobot(30, 50);
-  }
+  } 
+}
+
+void stepBack() {
+  controlRobot(-100, -100);
 }
 
 void act() {
+  printLidarData();
   if (millis() - lastReceiveTime <= timeoutDuration) {
     parseMessage(receivedData);
     adjustControlBasedOnDistance();
   } else {
     StopMotors();
-    BuzzerRobotSpecial();
   }
 }
 
@@ -188,4 +210,26 @@ void setup() {
   pinMode(IN_2, OUTPUT);
   pinMode(buzzerPin, OUTPUT);
 
-  STEERING.attach(STEERING_SE
+  STEERING.attach(STEERING_SERVO_PIN);
+
+  StopMotors();
+  controlRobot(0, 0);
+  BuzzerRobotStart();
+  Serial.println("on");
+}
+
+void loop() {
+  int switchValue = analogRead(SWITCH_PIN);
+
+  if (isSwitchOn(switchValue)) {
+    act();
+  } else {
+    StopMotors();
+    controlRobot(0, 0);
+    BuzzerRobotError();
+    Serial.println("ready");
+
+  }
+
+  delay(100);
+}
